db49d483155dd828c6a44be0f1cee84cbb5c6517
ChuchyX <jesuspenax11@gmail.com>
2023-04-06 18:03:14 -0400

    filter by Region Complete

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,13 +9,20 @@ import { CountriesService } from 'src/app/services/countries.service';
 })
 export class HomeComponent {
 
+  countriesCopy: Country[] = [];
   countries: Country[] = [];
   searchText!: string;
 
   constructor(private countriesService: CountriesService){
     this.countriesService.getCountries().subscribe(r => {
       console.log(r);
+      this.countriesCopy = r;
       this.countries = r;
     })
+  }
+
+  filterByRegion(region: string)
+  {
+    this.countries = this.countriesCopy.filter(c => c.region === region);
   }
 }
