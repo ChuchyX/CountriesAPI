7de81c1850ebf9dc70a823794b32e1b229fe9065
ChuchyX <jesuspenax11@gmail.com>
2023-04-06 14:17:09 -0400

    filters maquetado complete. Complete dark mode

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  theme = true;
+
+  ngOnInit(): void {
+    if(this.theme)
+    {
+      document.documentElement.setAttribute("theme", "dark");
+    }
+    else
+    {
+      document.documentElement.setAttribute("theme", "light");
+    }
+  }
+
+  changeTheme()
+  {
+    this.theme = !this.theme;
+    this.ngOnInit();
+  }
 
 }
