d2bf44e9f76f8fef386eb8704995a75461363c99
ChuchyX <jesuspenax11@gmail.com>
2023-04-10 20:49:53 -0400

    fixed errors

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,26 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
   theme = true;
 
   ngOnInit(): void {
-    if(this.theme)
-    {
-      document.documentElement.setAttribute("theme", "dark");
-    }
-    else
-    {
-      document.documentElement.setAttribute("theme", "light");
+    if (localStorage.getItem('theme') === 'dark') this.theme = true;
+    if (localStorage.getItem('theme') === 'light') this.theme = false;
+    if (this.theme) {
+      document.documentElement.setAttribute('theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('theme', 'light');
+      localStorage.setItem('theme', 'light');
     }
   }
 
-  changeTheme()
-  {
+  changeTheme() {
     this.theme = !this.theme;
+    if (localStorage.getItem('theme') === 'dark')
+      localStorage.setItem('theme', 'light');
+    else localStorage.setItem('theme', 'dark');
     this.ngOnInit();
   }
-
 }
